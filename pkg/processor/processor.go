1e5442328873d3008e71e1a67c4e9a500c0d66b8
Matt Farmer <matt@frmr.me>
2018-04-14 00:26:44 -0400

    Add basic append support

--- pkg/processor/processor.go
+++ pkg/processor/processor.go
@@ -1,7 +1,6 @@
 package processor
 
 import (
-	//"fmt"
 	"reflect"
 	"strings"
 )
@@ -10,8 +9,45 @@ const nestingSeparator = "."
 
 const anyChildSelector = "*"
 
-const actionSet = "="
 const actionAppend = "+"
+
+func doAppend(head interface{}, value interface{}, data map[interface{}]interface{}) map[interface{}]interface{} {
+	switch typedTemplateValue := data[head].(type) {
+	case string:
+		switch typedNewValue := value.(type) {
+		case string:
+			finalValue := typedTemplateValue + typedNewValue
+			data[head] = finalValue
+		}
+
+	case []interface{}:
+		switch typedNewValue := value.(type) {
+		case []interface{}:
+			finalValue := append(typedTemplateValue, typedNewValue...)
+			data[head] = finalValue
+		}
+	}
+
+
+	return data
+}
+
+func doSet(head interface{}, value interface{}, data map[interface{}]interface{}) map[interface{}]interface{} {
+	switch typedHead := head.(type) {
+	case string:
+		if strings.HasSuffix(typedHead, actionAppend) {
+			typedHeadWithoutSuffix := strings.TrimSuffix(typedHead, actionAppend)
+			var newHeadIntf interface{}
+			newHeadIntf = typedHeadWithoutSuffix
+
+			data = doAppend(newHeadIntf, value, data)
+		} else {
+			data[head] = value
+		}
+	}
+
+	return data
+}
 
 /**
  * Process an individual selector value
@@ -31,7 +67,7 @@ func ProcessSelector(
 			keys := reflect.ValueOf(data).MapKeys()
 			for _, key := range keys {
 				if remainLen == 0 {
-					data[key.Interface()] = value
+					data = doSet(key.Interface(), value, data)
 				} else {
 					switch nextData := data[key.Interface()].(type) {
 					case map[interface{}]interface{}:
@@ -51,8 +87,7 @@ func ProcessSelector(
 
 		default:
 			if remainLen == 0 {
-				data[head] = value
-				return data
+				return doSet(head, value, data)
 			} else if data[head] != nil {
 				switch nextData := data[head].(type) {
 				case map[interface{}]interface{}:
@@ -73,6 +108,9 @@ func ProcessSelector(
 	return nil
 }
 
+/**
+ * Run the provided template through the provided transform.
+ */
 func Process(
 	template map[interface{}]interface{},
 	transform map[interface{}]interface{},
